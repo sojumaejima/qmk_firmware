d3efc758cd2ae3d74c45bba045ccbd96ce8561bb
Soju Maejima <113083586+sojumaejima@users.noreply.github.com>
2023-10-05 21:49:15 +0900

    fix info.json

--- keyboards/aki27/cocot46plus/config.h
+++ keyboards/aki27/cocot46plus/config.h
@@ -21,9 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0x1727
-#define PRODUCT_ID   0x0003
-#define DEVICE_VER   0x0001
+//#define VENDOR_ID    0x1727
+//#define PRODUCT_ID   0x0003
+//#define DEVICE_VER   0x0001
 //#define MANUFACTURER aki27
 //#define PRODUCT      cocot46plus
 
